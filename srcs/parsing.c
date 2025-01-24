b6ca88322c6c0e338fdc84476f763878b9d8f8fd
Matthieu Galvez <mgalvez@paul-f5Ar3s5.clusters.42paris.fr>
2025-01-24 11:39:37 +0100

    norme parsing

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: mgalvez <mgalvez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 18:24:07 by mgalvez           #+#    #+#             */
-/*   Updated: 2025/01/23 19:13:08 by mgalvez          ###   ########.fr       */
+/*   Updated: 2025/01/24 11:38:06 by mgalvez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,8 @@ static int	parse_cmd(char **line, int cmd_len, int args_len)
 	cmd.args = ft_calloc(args_len, sizeof(char *));
 	if (!cmd.args)
 		return (1);
+	cmd.fd_in = 1;
+	cmd.fd_out = 0;
 	i = 0;
 	j = 0;
 	while (i < cmd_len)
@@ -63,6 +65,22 @@ static int	parse_cmd(char **line, int cmd_len, int args_len)
 	return (0);
 }
 
+static void	find_operator(char **line, int *i, int *cmd_len, int *args_len)
+{
+	if (ft_strncmp(line[*i], "|", ft_strlen(line[*i])))
+		*cmd_len += 1;
+	else
+		return ;
+	if (ft_strncmp(line[*i], "<", ft_strlen(line[*i]))
+		&& ft_strncmp(line[*i], ">", ft_strlen(line[*i]))
+		&& ft_strncmp(line[*i], "<<", ft_strlen(line[*i]))
+		&& ft_strncmp(line[*i], ">>", ft_strlen(line[*i])))
+		*args_len += 1;
+	else
+		i++;
+	i++;
+}
+
 void	parse_line(char **line)
 {
 	int	i;
@@ -73,23 +91,12 @@ void	parse_line(char **line)
 	cmd_len = 0;
 	args_len = 0;
 	while (line[i])
-	{
-		if (ft_strncmp(line[i], "|", ft_strlen(line[i])))
-			cmd_len++;
-		else
-			break ;
-		if (ft_strncmp(line[i], "<", ft_strlen(line[i]))
-			&& ft_strncmp(line[i], ">", ft_strlen(line[i]))
-			&& ft_strncmp(line[i], "<<", ft_strlen(line[i]))
-			&& ft_strncmp(line[i], ">>", ft_strlen(line[i])))
-			args_len++;
-		else
-			i++;
-		i++;
-	}
+		find_operator(line, &i, &cmd_len, &args_len);
+	i++;
 	if (parse_cmd(line, cmd_len, args_len))
 	{
 		ft_putstr("Error\nFailed to initiate struct\n", 2);
 		exit (EXIT_FAILURE);
 	}
+	parse_line(&line[i]);
 }
