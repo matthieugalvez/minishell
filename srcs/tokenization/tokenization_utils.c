3936ed8f6fe9d412767787a9a052ddef3524f7a1
Matthieu Galvez <mgalvez@paul-f5Ar3s3.clusters.42paris.fr>
2025-02-03 15:12:53 +0100

    debug heredoc

--- srcs/tokenization/tokenization_utils.c
+++ srcs/tokenization/tokenization_utils.c
@@ -6,7 +6,7 @@
 /*   By: mmanuell <mmanuell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 17:44:51 by mmanuell          #+#    #+#             */
-/*   Updated: 2025/02/03 14:02:22 by mgalvez          ###   ########.fr       */
+/*   Updated: 2025/02/03 14:42:18 by mgalvez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,32 +41,47 @@ void	unquote_args(t_cmd *cmd, t_data *data)
 	print_linetab("After unquote", cmd->args);
 }
 
-int	parse_cmd(t_cmd *cmd, char **line, int cmd_len, int args_len)
+static int	check_redirect(t_cmd *cmd, char **line, int cmd_len)
 {
-	int		i;
-	int		j;
+	int	i;
+	int	j;
 
-	cmd->args = ft_calloc(sizeof(char *), args_len + 1);
-	if (!cmd->args)
-		return (1);
 	i = 0;
 	j = 0;
 	while (i < cmd_len)
 	{
 		i += get_redirect_fd(cmd, line, i);
+		if (i < 0)
+		{
+			ft_freetab(cmd->args);
+			return (-1);
+		}
 		if (i > cmd_len)
 			break ;
 		cmd->args[j] = ft_strdup(line[i]);
 		if (!cmd->args[j])
 		{
 			ft_freetab(cmd->args);
-			return (1);
+			return (-1);
 		}
 		j++;
 		i++;
 	}
-	cmd->args[j] = 0;
-	cmd->argc = j;
+	return (j);
+}
+
+int	parse_cmd(t_cmd *cmd, char **line, int cmd_len, int args_len)
+{
+	int	i;
+
+	cmd->args = ft_calloc(sizeof(char *), args_len + 1);
+	if (!cmd->args)
+		return (1);
+	i = check_redirect(cmd, line, cmd_len);
+	if (i < 0)
+		return (1);
+	cmd->args[i] = 0;
+	cmd->argc = i;
 	return (0);
 }
 
